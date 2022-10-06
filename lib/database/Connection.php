0a76f43c8a9deebb8ff2170a7c5763c980dcf18f
padilhaigor <igor.padilha2004@gmail.com>
2022-10-05 21:00:36 -0300

    setup dotenv

--- lib/database/Connection.php
+++ lib/database/Connection.php
@@ -1,16 +1,25 @@
 <?php
     // conexão com o banco de dados
+    require_once(__DIR__ . "/../../vendor/autoload.php");
+
+    $envFileDir = __DIR__ . "/../../";
+
+    Dotenv\Dotenv::createImmutable($envFileDir)->load();
+
     abstract class Connection{
         private static $conn;
 
         public static function getConn(){
             if(self::$conn == null){
                 try{
-                    self::$conn = new PDO('mysql:host=127.0.0.1:33061;dbname=acesso;', 'root', "baldevazio7"); // mudar de acordo com a configuração do BD
+                    $host = $_ENV["DB_HOST"];
+                    $port = $_ENV["DB_PORT"];
+                    $database = $_ENV["DB_NAME"];
+                    $user = $_ENV["DB_USER"];
+                    $password = $_ENV["DB_PASSWORD"];
+                    self::$conn = new PDO("mysql:host=$host;port=$port;dbname=$database;", $user, $password); // mudar de acordo com a configuração do BD
                 } catch(Exception $e){
-                    echo $e->getMessage();
-                    exit();
-                    self::$conn = new PDO('mysql:host=127.0.0.1;port=3308;dbname=a;', 'root', '');
+                   die($e->getMessage());
                 }
 
             }
